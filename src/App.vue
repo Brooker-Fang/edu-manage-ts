fd62bb7740a57dbf3c454909817b5f2931fcb7b3
comedyOfKing <f_hhong@163.com>
2020-10-28 16:51:08 +0800

    create layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    app
+    <!-- app
     <el-button type="text">button</el-button>
-    <router-view/>
+    <router-view/> -->
+    <Layout/>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-
+import Layout from '@/components/layout/index.vue'
 export default Vue.extend({
-  name: 'App'
+  name: 'App',
+  components: {
+    Layout,
+  },
 })
 </script>
 <style lang="scss" scoped>
---
+++ src/components/layout/index.vue
@@ -0,0 +1,34 @@
+<template>
+ <el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</template>
+<script lang='ts'>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Layout',
+  data () {
+    return {
+    }
+  },
+})
+</script>
+<style lang='scss' scoped>
+.el-container {
+  min-height: 100vh;
+  min-width: 980px;
+}
+.el-aside {
+  background: #d3dce6;
+}
+.el-header {
+  background: #b3c0d1;
+}
+.el-main {
+  background: #e9eef3;
+}
+</style>
