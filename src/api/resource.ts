fcd02c0989bb2b15287a836902e1973fd71b2a40
comedyOfKing <f_hhong@163.com>
2020-11-04 22:05:05 +0800

    资源模块api提交

---
+++ src/api/resource.ts
@@ -0,0 +1,42 @@
+// 菜单
+import request from '@/utils/request'
+export const createRes = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/resource/category/saveOrderUpdate',
+    data,
+  })
+}
+export const getList = (data: object) => {
+  return request({
+    method: 'POST',
+    url: '/boss/resource/getResourcePages',
+    data
+  })
+}
+export const getRes = (id: number | string) => {
+  return request({
+    method: 'GET',
+    url: `/boss/resource/${id}`,
+  })
+}
+export const deleteRes = (id: number) => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/resource/${id}`,
+  })
+}
+
+// 资源分类
+export const getResTypeList = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/resource/category/getAll',
+  })
+}
+export const deleteResType = (id: number) => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/resource/category/${id}`,
+  })
+}
--- src/views/resource/index.vue
+++ src/views/resource/index.vue
@@ -1,16 +1,93 @@
 <template>
- <div>
-   Resource
- </div>
+  <el-card class="box-card">
+    <div slot="header"
+         class="clearfix">
+      <el-button type="default"
+                 @click="create">添加菜单</el-button>
+    </div>
+    <el-table :data="list"
+              class="list-table"
+              style="width: 100%"
+              v-loading="loading">
+      <el-table-column prop="name"
+                       label="菜单名称"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="level"
+                       label="菜单级数"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="icon"
+                       label="前端图标">
+      </el-table-column>
+      <el-table-column prop="orderNum"
+                       label="排序">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button @click="edit(scope.row.id)"
+                     size="small">编辑</el-button>
+          <el-button @click="deleteRes(scope.row.id)"
+                     type="danger"
+                     size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 <script lang='ts'>
 import Vue from 'vue'
+import { getList, deleteRes } from '@/api/resource'
 export default Vue.extend({
   name: 'Resource',
   data () {
     return {
+      list: [],
+      loading: false,
+      searchParams: {
+        name: '',
+        startCreateTime: '',
+        url: '',
+        categoryId: 0,
+        endCreateTime: '',
+        current: 1,
+        size: 10,
+      },
     }
   },
+  created () {
+    this.getList()
+  },
+  methods: {
+    async getList () {
+      this.loading = true
+      try {
+        const res = await getList(this.searchParams)
+        this.list = res.data.data
+      } catch (error) {
+        this.$message.error(error.message)
+      } finally {
+        this.loading = false
+      }
+    },
+    create () {
+      this.$router.push({
+        name: 'menu-create'
+      })
+    },
+    edit (id: string) {
+      this.$router.push({
+        name: 'menu-create',
+        query: {
+          id
+        }
+      })
+    },
+    async deleteRes (id: number) {
+      await deleteRes(id)
+      this.getList()
+    }
+  }
 })
 </script>
 <style lang='scss' scoped>
