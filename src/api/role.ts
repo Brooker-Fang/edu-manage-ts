e01e4f0d528908c107d3901ec62bd00cfe2225c1
comedyOfKing <f_hhong@163.com>
2020-11-10 23:22:26 +0800

    角色管理api和布局

---
+++ src/api/role.ts
@@ -0,0 +1,35 @@
+// 菜单
+import request from '@/utils/request'
+export const createRole = (data: object) => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/saveOrUpdate',
+    data,
+  })
+}
+export const getList = (data: object) => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/getRolePages',
+    data
+  })
+}
+export const getRole = (id: number | string) => {
+  return request({
+    method: 'GET',
+    url: `/boss/role/${id}`,
+  })
+}
+export const deleteRole = (id: number) => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/role/${id}`,
+  })
+}
+export const allocateUserRoles = (data: object) => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/allocateUserRoles',
+    data
+  })
+}
--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,15 +1,115 @@
 <template>
- <div>
-   Role
- </div>
+  <el-card class="box-card">
+    <div slot="header"
+         class="clearfix">
+      <el-form :inline="true"
+               :model="searchParams"
+               class="demo-form-inline">
+        <el-form-item label="角色名称">
+          <el-input v-model="searchParams.name"
+                    placeholder="角色名称"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary"
+                     @click="getList">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div>
+      <el-button @click="create">添加角色</el-button>
+    </div>
+    <el-table :data="list"
+              class="list-table"
+              style="width: 100%"
+              v-loading="loading">
+      <el-table-column prop="id"
+                       label="编号"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="name"
+                       label="资源名称"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="url"
+                       label="资源路径">
+      </el-table-column>
+      <el-table-column prop="description"
+                       label="描述">
+      </el-table-column>
+      <el-table-column prop="createdTime"
+                       label="添加时间">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button @click="edit(scope.row.id)"
+                     size="small">编辑</el-button>
+          <el-button @click="deleteRes(scope.row.id)"
+                     type="danger"
+                     size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 <script lang='ts'>
 import Vue from 'vue'
+import { getList, deleteRole } from '@/api/role'
 export default Vue.extend({
   name: 'Role',
   data () {
     return {
+      total: 0,
+      list: [],
+      typeList: [],
+      loading: false,
+      searchParams: {
+        name: '',
+        startCreateTime: '',
+        code: '',
+        endCreateTime: '',
+        current: 1,
+        size: 10,
+      },
     }
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    async getList () {
+      this.loading = true
+      try {
+        const res = await getList(this.searchParams)
+        this.list = res.data.data.records
+        this.total = res.data.data.total
+      } catch (error) {
+        this.$message.error(error.message)
+      } finally {
+        this.loading = false
+      }
+    },
+    create () {
+      this.$router.push({
+        name: 'resource-create',
+      })
+    },
+    edit (id: string) {
+      this.$router.push({
+        name: 'resource-create',
+        query: {
+          id,
+        },
+      })
+    },
+    goResClassify () {
+      this.$router.push({
+        name: 'resource-type',
+      })
+    },
+    async deleteRes (id: number) {
+      await deleteRole(id)
+      this.getList()
+    },
   },
 })
 </script>
