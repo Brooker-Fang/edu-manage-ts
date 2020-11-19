129a41e38ef9ac9b8872e8c8038b3df3e975eff8
comedyOfKing <f_hhong@163.com>
2020-11-19 13:57:26 +0800

    课程管理列表查询

---
+++ src/api/course.ts
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+interface Course {
+  courseId: string | number
+  courseName: string
+  status: number
+}
+export const getList = (data: object) => {
+  return request({
+    method: 'POST',
+    url: '/boss/course/getQueryCourses',
+    data
+  })
+}
+export const createCourse = (data: object) => {
+  return request({
+    method: 'POST',
+    url: '/boss/course/saveOrUpdateCourse',
+    data,
+  })
+}
+export const changStatus = (data: Course) => {
+  return request({
+    method: 'GET',
+    url: '/boss/course/changeState',
+    params: {
+      courseId: data.courseId,
+      status: data.status
+    }
+  })
+}
+export const getCourseById = (courseId: number| string) => {
+  return request({
+    method: 'GET',
+    url: '/boss/course/getCourseById',
+    params: {
+      courseId,
+    }
+  })
+}
--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -1,16 +1,138 @@
 <template>
- <div>
-   Course
- </div>
+ <el-card class="box-card">
+    <div slot="header"
+         class="clearfix">
+      <el-form :inline="true"
+               :model="searchParams"
+               class="demo-form-inline">
+        <el-form-item label="课程名称">
+          <el-input v-model="searchParams.courseName"
+                    placeholder="填写课程名称"></el-input>
+        </el-form-item>
+        <el-form-item label="课程名称">
+          <el-select v-model="searchParams.status" placeholder="选择课程状态">
+            <el-option value="">全部</el-option>
+            <el-option :value="1">上架</el-option>
+            <el-option :value="0">下架</el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary"
+                     @click="search">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <el-table :data="list"
+              class="list-table"
+              style="width: 100%"
+              v-loading="loading">
+      <el-table-column prop="id"
+                       label="ID"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="courseName"
+                       label="课程名称"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="price"
+                       label="价格">
+      </el-table-column>
+      <el-table-column prop="sortNum"
+                       label="排序">
+      </el-table-column>
+      <el-table-column prop="status"
+                       label="状态">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button @click="changStatus(scope.row.id)"
+                     pageSize="small">上架</el-button>
+                     <el-button @click="editCourse(scope.row.id)"
+                     pageSize="small">编辑</el-button>
+                     <el-button @click="editCourse(scope.row.id)"
+                     pageSize="small">内容管理</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog :title="'分配角色'"
+               :visible="show"
+               width="400px"
+               left>
+      <el-form ref="form"
+               :model="form"
+               label-width="80px"
+               class="form-style">
+               <el-form-item>
+        <el-select v-model="form.roleIdList" multiple placeholder="请选择">
+          <el-option
+            v-for="item in roleList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id">
+          </el-option>
+        </el-select>
+               </el-form-item>
+        <el-button type="default"
+                   class="login-btn"
+                   @click="show = false"
+                   >取消</el-button>
+        <el-button type="primary"
+                   class="login-btn"
+                   @click="show = false"
+                   :loading="loading">保存</el-button>
+      </el-form>
+    </el-dialog>
+  </el-card>
 </template>
 <script lang='ts'>
 import Vue from 'vue'
+import { getList } from '@/api/course'
+import dayjs from 'dayjs'
 export default Vue.extend({
-  name: 'Course',
+  name: 'User',
   data () {
     return {
+      dayjs: dayjs,
+      loading: false,
+      total: 0,
+      list: [],
+      roleList: [],
+      show: false,
+      searchParams: {
+        courseName: '',
+        status: '',
+        currentPage: 1,
+        pageSize: 10,
+      },
+      form: {
+        roleIdList: [],
+        userId: ''
+      }
     }
   },
+  created () {
+    this.getList(this.searchParams)
+  },
+  methods: {
+    search () {
+      this.getList(this.searchParams)
+    },
+    async getList (searchParams: object) {
+      this.loading = true
+      try {
+        const res = await getList(searchParams)
+        this.list = res.data.data.records
+        this.total = res.data.data.total
+      } catch (error) {
+        this.$message.error(error.message)
+      } finally {
+        this.loading = false
+      }
+    },
+    editCourse (id: number | string) {
+      console.log(id)
+    },
+  }
 })
 </script>
 <style lang='scss' scoped>
